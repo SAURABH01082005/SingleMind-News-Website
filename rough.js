4153d399fc88b6dcf54a59f7abfe343e2136074f
Saurabh <saurabhyadavprofile@gmail.com>
2025-10-03 11:36:59 +0530

    date 10/3/25

--- rough.js
+++
@@ -1,6 +0,0 @@
-myfunc()
-async function myfunc(){
-    k=await fetch("./keywords.json").catch(err=>{console.log("there is error in fetching keywords.json file , error is :",err)})
-    data=await k.json()
-    console.log("the json file  is : ",data)
-}
